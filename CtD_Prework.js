8164524958a82bb13ac34be534b2b856071869ba
jchen577 <95502719+jchen577@users.noreply.github.com>
2025-12-05 16:39:07 -0800

    Comments, Webpage Styling, Readme Additions

--- CtD_Prework.js
+++ CtD_Prework.js
@@ -1,26 +1,29 @@
 let weatherData;
 let params;
-let lat;
-let long;
+let lat = 37.7749; //Default to San Francisco coordinates
+let long = -122.4194;
+let setLoc = false;
 
-async function getLocation(){//Get current location of user
-  return new Promise((resolve,reject)=>{
+async function getLocation() {
+  //Get current location of user
+  return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
-      (position) =>{
+      (position) => {
         lat = position.coords.latitude;
         long = position.coords.longitude;
+        setLoc = true;
         resolve(position);
       },
-      (error) =>{
-        console.error("No location found",error);
-        reject(error);
-      }
-    ); 
+      (error) => {
+        console.error("No location found", error);
+        resolve(error);
+      },
+    );
   });
 }
 
 async function requestWeather() {
-  await getLocation();//Using user's location, call upon the meteo API
+  await getLocation(); //Using user's location, call upon the meteo API
   params = {
     latitude: lat,
     longitude: long,
@@ -39,7 +42,7 @@ async function requestWeather() {
 
   const response = await fetch(url);
   const data = await response.json();
-  console.log("Raw API Data: ", data);
+  //console.log("Raw API Data: ", data);
 
   weatherData = {
     current: {
@@ -55,31 +58,65 @@ async function requestWeather() {
       temperature_2m: data.hourly.temperature_2m,
     },
   };
-  console.log("Formatted weatherData:", weatherData);
+  //console.log("Formatted weatherData:", weatherData);
   return weatherData;
 }
 
-async function returnCurrentTemp(){
-  await requestWeather()
-  document.getElementById("tempOutput").textContent =
-    "The temparture is: " + weatherData.current.temperature_2m + "°C";
+async function returnCurrentTemp() {
+  //Change the output of the temperature page
+  await requestWeather();
+  if (setLoc) {
+    document.getElementById("tempOutput").textContent =
+      "The temperature near you is : " +
+      weatherData.current.temperature_2m +
+      "°C" +
+      " or : " +
+      ((weatherData.current.temperature_2m * 9) / 5 + 32).toFixed(2) +
+      "°F";
+  } else {
+    document.getElementById("tempOutput").textContent =
+      "The temperature at San Francisco is : " +
+      weatherData.current.temperature_2m +
+      "°C" +
+      " or : " +
+      ((weatherData.current.temperature_2m * 9) / 5 + 32).toFixed(2) +
+      "°F";
+  }
 }
 
-async function returnCurrentConditions(){
-  await requestWeather()
-  let rain = "not"
-  let snow = "not"
-  if(weatherData.current.rain == true){
-    rain = ""
+async function returnCurrentConditions() {
+  //Change the otput of the conditions page
+  await requestWeather();
+  let rain = "not";
+  let snow = "not";
+  if (weatherData.current.rain == true) {
+    rain = "";
   }
-  if(weatherData.current.snow == true){
-    snow = ""
+  if (weatherData.current.snow == true) {
+    snow = "";
   }
-  document.getElementById("conditionsOutput").textContent =
-    "It is currently " + rain + " raining!\nAnd it is " + snow + " snowing!";
+  if (setLoc) {
+    document.getElementById("conditionsOutput").textContent =
+      "It is currently " +
+      rain +
+      " raining and it is " +
+      snow +
+      " snowing near you!";
+  } else {
+    document.getElementById("conditionsOutput").textContent =
+      "It is currently " +
+      rain +
+      " raining and it is " +
+      snow +
+      " snowing in San Francisco!";
+  }
 }
 
 function returnCurrentLoc() {
   document.getElementById("outputLoc").textContent =
-    "The weather in your area of {Latitude: " + params.latitude+ " Longitude: " + params.longitude + "}";
+    "The weather in your area of {Latitude: " +
+    params.latitude +
+    " Longitude: " +
+    params.longitude +
+    "}";
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CTD_Prework
+
+# Title: Weathering For You
+
+# Author: Jacky Chen
+
+# This project uses the meteo-api to help the user know the weather conditions and temperature in their area or at San Francisco
+
+## Instructions: Open the webpage and click on the "temperature" or "conditions" button to be redirected. Once redirected, please allow the webpage to access your location or else it will default to San Francisco.
